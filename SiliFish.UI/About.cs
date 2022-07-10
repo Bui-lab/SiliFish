d900b0a49904f1a4c69a7e165aafd4fff1e11221
EmineTopcu <50247946+EmineTopcu@users.noreply.github.com>
2022-07-10 07:46:52 -0400

    Build date to About box "Stop run" ability to cancel wrongly started long runs

--- SiliFish.UI/About.cs
+++ SiliFish.UI/About.cs
@@ -4,20 +4,21 @@ namespace SiliFish.UI
 {
     public partial class About : Form
     {
-        //TODO find last compile time
         static DateTime dateUI = new(2022, 6, 13);
         static DateTime dateEngine = new(2022, 6, 13);
         public About()
         {
             InitializeComponent();
-
+            //https://stackoverflow.com/questions/1600962/displaying-the-build-date
+            //dateUI = DateTime.Parse(Properties.Resources.BuildDate); //There is a problem in getting the date info from UI.Resources
+            dateEngine = DateTime.Parse(SiliFish.Properties.Resources.BuildDate);
             Version versionUI = Assembly.GetExecutingAssembly().GetName().Version;
-            lVersionWindows.Text = $"Version: {versionUI}";
-            lBuiltOn.Text = $"Built on {dateUI:d}";
-            //Version versionEngine = typeof(SwimmingModel).Assembly.GetName().Version;
-            //lVersionEngine.Text = string.Format("Engine version: {0}.{1}, built on {2}", versionEngine.Major, versionEngine.Minor, dateEngine.ToString("d"));
+            lVersionWindows.Text = string.Format("UI version: {0}.{1}", versionUI.Major, versionUI.Minor);
+            //lVersionWindows.Text = string.Format("UI version: {0}.{1}, built on {2}", versionUI.Major, versionUI.Minor, dateUI.ToString("d"));
+            Version versionEngine = typeof(SwimmingModel).Assembly.GetName().Version;
+            lVersionEngine.Text = string.Format("Engine version: {0}.{1}, built on {2}", versionEngine.Major, versionEngine.Minor, dateEngine.ToString("d"));
         }
-            public void SetTimer(int ms)
+        public void SetTimer(int ms)
         {
             timerAbout.Interval = ms;
             timerAbout.Enabled = true;
