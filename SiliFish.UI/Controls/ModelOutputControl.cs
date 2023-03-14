e2a6e175a8f77be0863f47c03c016fe0959977e5
EmineTopcu <50247946+EmineTopcu@users.noreply.github.com>
2023-03-13 21:23:26 -0400

    .

--- SiliFish.UI/Controls/ModelOutputControl.cs
+++ SiliFish.UI/Controls/ModelOutputControl.cs
@@ -99,6 +99,7 @@ namespace SiliFish.UI.Controls
 
         public void SetRunningModel(RunningModel model)
         {
+            if (model == null) return;
             RunningModel = model;
             PopulatePlotPools();
             int NumberOfSomites = RunningModel.ModelDimensions.NumberOfSomites;
@@ -715,8 +716,8 @@ namespace SiliFish.UI.Controls
         private void RenderIn2D()
         {
             if (RunningModel == null) return;
-            TwoDRenderer modelGenerator = new();
-            string html = modelGenerator.Create2DRendering(RunningModel, RunningModel.CellPools, (int)webView2DRender.Width, webView2DRender.Height,
+            TwoDModelGenerator modelGenerator = new();
+            string html = modelGenerator.Create2DModel(RunningModel, RunningModel.CellPools, (int)webView2DModel.Width, webView2DModel.Height,
                 showGap: cb2DGapJunc.Checked, showChem: cb2DChemJunc.Checked);
             webView2DRender.NavigateTo(html, GlobalSettings.TempFolder, ref tempFile);
 
@@ -774,6 +775,7 @@ namespace SiliFish.UI.Controls
                 ThreeDRenderer threeDRenderer = new();
                 string html = threeDRenderer.RenderIn3D(RunningModel, RunningModel.CellPools,
                     somiteRange: cb3DAllSomites.Checked ? "All" : e3DSomiteRange.Text,
+                    webView3DModel.Width, webView3DModel.Height,
                     showGap: cb3DGapJunc.Checked, showChem: cb3DChemJunc.Checked);
                 webView3DRender.NavigateTo(html, GlobalSettings.TempFolder, ref tempFile);
             }
