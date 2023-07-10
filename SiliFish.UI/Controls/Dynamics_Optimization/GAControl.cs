5f39e7c08e46e7ae336a034131ba7effebcf29b8
EmineTopcu <50247946+EmineTopcu@users.noreply.github.com>
2023-07-09 21:55:29 -0400

    Misc tweaks to GA

--- SiliFish.UI/Controls/Dynamics_Optimization/GAControl.cs
+++ SiliFish.UI/Controls/Dynamics_Optimization/GAControl.cs
@@ -100,7 +100,7 @@ namespace SiliFish.UI.Controls
                 solver = iterSolver;
                 exhaustiveSolverIterator++;
                 CoreSolverOutput currentOutput = solver.Optimize();
-                if (exhaustiveSolverOutput == null || exhaustiveSolverOutput.BestFitness < currentOutput.BestFitness)
+                if (exhaustiveSolverOutput == null || exhaustiveSolverOutput.BestFitness <= currentOutput.BestFitness)
                 {
                     exhaustiveSolverOutput = currentOutput;
                     exhaustiveBestSolver = solver;
@@ -119,7 +119,8 @@ namespace SiliFish.UI.Controls
 
                     Invoke(TriggerOptimizationLoop);
                 }
-                if (exhaustiveSolverOutput?.BestFitness >= iterSolver.Settings.TargetFitness - GlobalSettings.Epsilon)
+                if (exhaustiveSolverOutput?.BestFitness >= iterSolver.Settings.TargetFitness - GlobalSettings.Epsilon
+                    && exhaustiveBestParameters.Count == GlobalSettings.GeneticAlgorithmExhaustiveSolutionCount)
                     break;
             }
             solver = exhaustiveBestSolver;
@@ -330,6 +331,7 @@ namespace SiliFish.UI.Controls
             string progress = solver.ProgressText;
             if (exhaustiveSolverIterator > 0)
                 progress = $"{progress}\r\n" +
+                    $"~.~.~.~.~.~.~.~.~.~.~.~.~.~\r\n" +
                     $"Exhaustive search {exhaustiveSolverIterator}/{exhaustiveSolverList.Count}\r\n" +
                     $"Best overall fitness: {exhaustiveSolverOutput?.BestFitness}";
             optimizationProgress.Progress = solver.Progress;
